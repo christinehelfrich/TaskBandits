068f59d1a1706f0c14371d99950f5754ddb0732d
Christine <christine.helfrich1@gmail.com>
2024-03-07 08:15:20 -0800

    remove boilerplate frontend code

--- backend/routes/ProfileRoute.js
+++ backend/routes/ProfileRoute.js
@@ -1,8 +1,11 @@
 const {Router} = require("express")
-const {getProfiles} = require("../controllers/ProfileControllers")
+const {getProfiles, saveProfile, deleteProfile, updateProfile} = require("../controllers/ProfileControllers")
 
 const router = Router()
 
-router.get("/get", getProfiles);
+router.get("/profiles", getProfiles);
+router.post("/profile", saveProfile);
+router.put("/profile/:id", updateProfile)
+router.delete("/profile/:id", deleteProfile)
 
 module.exports = router
