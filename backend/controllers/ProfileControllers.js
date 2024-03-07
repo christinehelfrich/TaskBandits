3255b225390acfee2cc1ee7ebb2e122365490dff
Christine <christine.helfrich1@gmail.com>
2024-03-07 08:02:29 -0800

    add delete and update profile actions

--- backend/controllers/ProfileControllers.js
+++ backend/controllers/ProfileControllers.js
@@ -5,7 +5,7 @@ module.exports.getProfiles = async (req, res) => {
     res.send(profiles)
 }
 
-module.exports.saveProfile = async (req, res) => {
+module.exports.saveProfile = (req, res) => {
     const {profile} = req.body
 
     ProfileModel.create({profile})
@@ -13,6 +13,29 @@ module.exports.saveProfile = async (req, res) => {
         console.log("Saved Successfully...")
         res.status(201).send(data)
     }).catch((err) => {
+        console.log(err);
+        res.send({error: err, msg: "Something went wrong!"});
+    })
+}
+
+module.exports.updateProfile = (req, res) => {
+    const {id} = req.params
+    const {profile} = req.body
+
+    ProfileModel.findByIdAndUpdate(id, {profile})
+    .then(() => res.send("Updated Successfully..."))
+    .catch((err) => {
+        console.log(err);
+        res.send({error: err, msg: "Something went wrong!"});
+    })
+}
+
+module.exports.deleteProfile = (req, res) => {
+    const {id} = req.params
+
+    ProfileModel.findByIdAnddelete(id)
+    .then(() => res.send("Deleted Successfully..."))
+    .catch((err) => {
         console.log(err);
         res.send({error: err, msg: "Something went wrong!"});
     })
