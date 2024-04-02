c1d532bd58189253fcc04ac26ffc3c0ffb5b1b88
Christine <christine.helfrich1@gmail.com>
2024-04-02 11:00:10 -0700

    remove unnecessary logs

--- backend/middleware/profileTesting.test.js
+++ backend/middleware/profileTesting.test.js
@@ -2,7 +2,6 @@ const ProfileMiddleware = require('./ProfileMiddleware')
 
 
 test("Returns a false for a password that doesn't have at least 8 characters", () => {
-    console.log("HERE -> ", ProfileMiddleware.validatePassword)
     expect(ProfileMiddleware.validatePassword('abC12&')).toBe(false)
 }) 
 
@@ -17,3 +16,5 @@ test("Returns a false for a password that doesn't have at least an upper and low
 test("Returns a false for a password that doesn't have at least a number", () => {
     expect(ProfileMiddleware.validatePassword('abcdef!@#$%')).toBe(false)
 }) 
+
+
