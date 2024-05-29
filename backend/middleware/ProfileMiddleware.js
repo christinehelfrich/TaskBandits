256b3239321b2f44c27079952fc0d4bfdec55dbd
Christine <christine.helfrich1@gmail.com>
2024-05-29 09:49:35 -0700

    add password encryption

--- backend/middleware/ProfileMiddleware.js
+++ backend/middleware/ProfileMiddleware.js
@@ -1,6 +1,8 @@
 const express = require("express");
+const bcrypt = require("bcrypt")
+const saltRounds = 10
 
-module.exports.validatePassword = (password) => {
+const validatePassword = (password) => {
     let pattern = /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,}$/
     return pattern.test(password);
 }
@@ -13,4 +15,18 @@ module.exports.isPasswordValid = (req, res, next) => {
     }else {
         res.status(400).send({error: 'error', msg: "Password wasn't long enough!"});
     }
+}
+
+module.exports.encryptPassword = (req, res, next) => {
+    bcrypt
+    .genSalt(saltRounds)
+    .then(salt => {
+      return bcrypt.hash(req.body.profile.password, salt)
+    })
+    .then(hash => {
+      req.body.profile.password = hash
+      next()
+    })
+    .catch(err => res.status(400).send({error: 'error', msg: "Could not encrypt password, check ByCrypt"}))
+
 }
